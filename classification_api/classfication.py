e64454ab3ae03db0f40fc26a2dcad7d08d01f88f
Akhil <55911943+nvakhilnair@users.noreply.github.com>
2021-12-23 17:16:33 +0530

    Update classfication.py

--- classification_api/classfication.py
+++ classification_api/classfication.py
@@ -1,11 +1,11 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
+global analyser
+analyser = SentimentIntensityAnalyzer()
 def classify(text):
     if(str(type(text)) == "<class 'dict'>"):
         text = list(text.values())[0]
     else:
         pass
-    analyser = SentimentIntensityAnalyzer()
     output = analyser.polarity_scores(text)
     if output['compound'] >= 0.05 :
         overall_rating = "Positive"
