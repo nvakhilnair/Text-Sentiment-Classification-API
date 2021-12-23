720011a759063cc9dbefc804e5a5d31a53d90257
Akhil <55911943+nvakhilnair@users.noreply.github.com>
2021-12-23 16:43:16 +0530

    Update settings.py

--- sentimental_classification/settings.py
+++ sentimental_classification/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'sentimental_classification.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
